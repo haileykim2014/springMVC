9a83dda6f876aac4251bb32d9bfe1bfb8c136b37
YOUNGHWA KIM <haileykim2014@gmail.com>
2022-01-09 18:27:16 +0900

    요구사항 분석

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,18 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+
+@Data
+public class Item {
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;//수량이 null로 들어갈 수있다.
+    public Item() {
+    }
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
