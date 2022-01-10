ad2ae4ed0b511af2cfbc5f8cdb4670050c71a49b
YOUNGHWA KIM <haileykim2014@gmail.com>
2022-01-10 22:10:18 +0900

    상품 등록폼

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -8,7 +8,9 @@ public class Item {
     private String itemName;
     private Integer price;
     private Integer quantity;//수량이 null로 들어갈 수있다.
+
     public Item() {
+
     }
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -3,15 +3,15 @@ package hello.itemservice.web.basic;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
-
+import java.util.Map;
+@Slf4j
 @Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor//final이 붙은것에 자동으로 생성자생성
@@ -25,6 +25,26 @@ public class BasicItemController {
         model.addAttribute("items", items);
         return "basic/items";
     }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@RequestParam Map<String,Object> map) {//같은 url로 오더라도 post올겨우에만 호출된다
+        log.info("itemName={}",map.get("itemName"));
+        log.info("price={}",map.get("price"));
+        log.info("quantity={}",map.get("quantity"));
+        return "xxx";
+    }
     /**
      * 테스트용 데이터 추가
      */
@@ -34,10 +54,4 @@ public class BasicItemController {
         itemRepository.save(new Item("testB", 20000, 20));
     }
 
-    @GetMapping("/{itemId}")
-    public String item(@PathVariable Long itemId, Model model) {
-        Item item = itemRepository.findById(itemId);
-        model.addAttribute("item", item);
-        return "basic/item";
-    }
 }
